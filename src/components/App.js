30f68b5d63526dee039019e8cd5f07cc52a65976
Renato Camilio <renatomcamilio@gmail.com>
2016-04-21 00:02:37 -0700

    #If applied, this commit will... Include IndexRoute for rendering the RecipeList Component by default # Explain why this change is being made
    # Provide links to any relevant tickets, articles or other resources

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import Home from '../pages/Home';
 
-const App = () => (
+const App = ({ children }) => (
   <div className="pure-g">
     <div className="pure-u-2-24 pure-u-lg-4-24"></div>
     <div className="pure-u-20-24 pure-u-lg-16-24">
-      <Home />
+      {children}
     </div>
     <div className="pure-u-2-24 pure-u-lg-4-24"></div>
   </div>
